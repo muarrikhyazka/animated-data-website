13f836b29ae18e0fa1af96906b6ccd58bc7d6e19
MuarrikhYazka <37464135+MuarrikhYazka@users.noreply.github.com>
2023-09-07 21:39:54 +0700

    add opening

--- js/vertical-script.js
+++ js/vertical-script.js
@@ -1,48 +1,30 @@
-'use strict';
+let text1 = document.getElementById('text1');
+let ruangkendali = document.getElementById('ruangkendali');
+let kaca = document.getElementById('kaca');
+let text2 = document.getElementById('text2');
+let fusion = document.getElementById('fusion');
 
-var animatedElements = document.querySelectorAll('.animated');
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
 
-var debounce = function debounce(func) {
-  var wait = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 20;
-  var immediate = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
+    text1.style.marginTop = value * 2.5 + 'px';
+    if (value > 400) {
+        text1.style.opacity = 0;
+      } else {
+        text1.style.opacity = 1;
+      }
+    text2.style.marginTop = value * 2.5 + 'px';
+    if (value < 400) {
+        text2.style.opacity = 0;
+      } else {
+        text2.style.opacity = 1;
+      }
+    console.log(value);
+    fusion.style.top = value * -1.5 + 'px';
+    ruangkendali.style.top = value * -1.5 + 'px';
+    kaca.style.top = value * -1.5 + 'px';
 
-  var timeout = void 0;
-  return function () {
-    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
 
-    var context = undefined;
-    var later = function later() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
 
-var checkAnimation = function checkAnimation(e) { //Check if a scroll animated element is in view
+});
 
-  animatedElements.forEach(function (animatedElement) {
-    var slideInTrigger = window.scrollY + window.innerHeight - animatedElement.offsetHeight;
-
-    var isShowing = slideInTrigger > animatedElement.offsetTop;
-
-    if (isShowing) {
-      animatedElement.classList.remove('inactive'); //Unhide the element
-    } else {
-      animatedElement.classList.add('inactive'); //Hide the element
-    }
-  });
-};
-
-var hideAnimated = function hideAnimated(e) {
-  animatedElements.forEach( function(animatedElement) {
-    animatedElement.classList.add('inactive'); //Hide elements by default
-  })
-}
-
-window.addEventListener('scroll', debounce(checkAnimation, 20)); 
