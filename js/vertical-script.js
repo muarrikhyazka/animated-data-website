80cd333727557377ec42f9b4c83076f23be198b4
MuarrikhYazka <37464135+MuarrikhYazka@users.noreply.github.com>
2023-09-07 10:44:09 +0700

    upload assets

---
+++ js/vertical-script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var animatedElements = document.querySelectorAll('.animated');
+
+var debounce = function debounce(func) {
+  var wait = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 20;
+  var immediate = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
+
+  var timeout = void 0;
+  return function () {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    var context = undefined;
+    var later = function later() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+var checkAnimation = function checkAnimation(e) { //Check if a scroll animated element is in view
+
+  animatedElements.forEach(function (animatedElement) {
+    var slideInTrigger = window.scrollY + window.innerHeight - animatedElement.offsetHeight;
+
+    var isShowing = slideInTrigger > animatedElement.offsetTop;
+
+    if (isShowing) {
+      animatedElement.classList.remove('inactive'); //Unhide the element
+    } else {
+      animatedElement.classList.add('inactive'); //Hide the element
+    }
+  });
+};
+
+var hideAnimated = function hideAnimated(e) {
+  animatedElements.forEach( function(animatedElement) {
+    animatedElement.classList.add('inactive'); //Hide elements by default
+  })
+}
+
+window.addEventListener('scroll', debounce(checkAnimation, 20)); 
